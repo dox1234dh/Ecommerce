07edd8af716cce2f83840cc53d1586a81eaa51a0
nguyetmoon <nguyetmoon180499@gmail.com>
2022-05-16 17:02:00 +0700

    first commit

---
+++ store/views.py
@@ -0,0 +1,17 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html')
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html')
